bc4634414b34df8e625889ba5d6a9f6ef7dce21f
Maksim Bondarenkov <maksapple2306@gmail.com>
2023-12-28 13:16:14 +0300

    fix ci VIII

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,11 @@
 use std::process::Command;
 
+// msys2 ci only
+
 #[test]
 fn tarball() {
-    Command::new("sh ./.ci/tarball.sh")
+    Command::new("sh")
+        .arg("./.ci/tarball.sh")
         .status()
         .expect("failed to test");
 }
