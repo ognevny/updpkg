b6fe3c1c448aa1fcfaa89122156f3b5b15524b04
Maksim Bondarenkov <119937608+ognevnydemon@users.noreply.github.com>
2023-12-31 07:59:08 +0300

    small refactor

--- src/main.rs
+++ src/main.rs
@@ -49,41 +49,37 @@ fn main() {
         env::set_current_dir(dir).unwrap_or_else(|e| error!("couldn't change directory: {e}"));
     }
 
-    if !git {
-        if let Some(ver) = version {
-            info!("setting pkgver as {ver}, setting pkgrel as 1");
-            // assuming sed doesn't fail
-            match Command::new("sed")
-                .args([
-                    "-i",
-                    "-e",
-                    &format!("s|^pkgver=.*|pkgver={ver}|; s|^pkgrel=.*|pkgrel=1|"),
-                    "PKGBUILD",
-                ])
-                .status() {
-                    Ok(_) => (),
-                    Err(e) => error!("couldn't sed PKGBUILD: {e}"),
-                }
-        }
-    // bless clippy
-    } else if git {
-        if let Some(ver) = version {
-            info!("setting commit as {ver}, setting pkgrel as 1");
-            match Command::new("sed")
-                .args([
-                    "-i",
-                    "-e",
-                    &format!("s|^_commit=.*|_commit={ver}|; s|^pkgrel=.*|pkgrel=1|"),
-                    "PKGBUILD",
-                ])
-                .status() {
-                    Ok(_) => (),
-                    Err(e) => error!("couldn't sed PKGBUILD: {e}"),
-                }
-            if !make && !make_mingw {
-                warn!("you may need to run `makepkg` manually to update `pkgver`");
+    if !git && if let Some(ver) = version {
+        info!("setting pkgver as {ver}, setting pkgrel as 1");
+        match Command::new("sed")
+            .args([
+                "-i",
+                "-e",
+                &format!("s|^pkgver=.*|pkgver={ver}|; s|^pkgrel=.*|pkgrel=1|"),
+                "PKGBUILD",
+            ])
+            .status() {
+                Ok(_) => (),
+                Err(e) => error!("couldn't sed PKGBUILD: {e}"),
             }
+    } else if let Some(ver) = version {
+        info!("setting commit as {ver}, setting pkgrel as 1");
+        match Command::new("sed")
+            .args([
+                "-i",
+                "-e",
+                &format!("s|^_commit=.*|_commit={ver}|; s|^pkgrel=.*|pkgrel=1|"),
+                "PKGBUILD",
+            ])
+            .status() {
+                Ok(_) => (),
+                Err(e) => error!("couldn't sed PKGBUILD: {e}"),
+            }
+        if !make && !make_mingw {
+            warn!("you may need to run `makepkg` manually to update `pkgver`");
         }
+    } else if git {
+        warn!("commit SHA isn't specified");
     }
 
     info!("updating checksums");
@@ -96,13 +92,13 @@ fn main() {
         error!("can't invoke both `makepkg` and `makepkg-mingw`");
     } else if make {
         info!("using `makepkg` with flags {flags}");
-        match Command::new("sh").arg("makepkg").arg(&flags).status() {
+        match Command::new("sh").args(["makepkg", &flags]).status() {
             Ok(_) => (),
             Err(e) => error!("couldn't make package: {e}"),
         }
     } else if make_mingw {
         info!("using `makepkg-mingw` with flags {flags}");
-        match Command::new("sh").arg("makepkg-mingw").arg(&flags).status() {
+        match Command::new("sh").args(["makepkg-mingw", &flags]).status() {
             Ok(_) => (),
             Err(e) => error!("couldn't make package: {e}"),
         }
