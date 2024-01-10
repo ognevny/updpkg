92b5cc312284db77e734402fdc2205792fe8742c
ognevnydemon <maksapple2306@gmail.com>
2024-01-10 16:58:58 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use {
     clap::Parser,
+    env_logger::{Builder, Env},
     log::{error, info, warn},
-    std::{env, fs, path::PathBuf, process::Command},
+    std::{
+        env, fs,
+        io::Error,
+        path::PathBuf,
+        process::{Command, ExitStatus},
+    },
 };
 
 #[derive(Debug, Parser)]
@@ -32,8 +38,14 @@ struct Args {
     rm: Option<Vec<String>>,
 }
 
+fn sed(re: &str) -> Result<ExitStatus, Error> {
+    Command::new("sed")
+        .args(["-i", "-e", &re, "PKGBUILD"])
+        .status()
+}
+
 fn main() {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let args = Args::parse();
     let (ver, dir, make, make_mingw, git, rm) = (
@@ -52,71 +64,66 @@ fn main() {
         for file in files.iter() {
             info!("removing {file} from recipe");
             let replaced = file.replace('.', "\\.");
-            match Command::new("sed")
-                .args([
-                    "-i",
-                    "-e",
-                    // don't know how to optimize it
-                    &format!("s|^.*{replaced}\\s*||g; s|^\")|)|g; s|^\"$||g"),
-                    "PKGBUILD",
-                ])
-                .status()
-            {
+            match sed(&format!(r#"s|^.*{replaced}\s*||g; s|^")|)|g; s|^"$||g"#)) {
                 Ok(_) => (),
-                Err(e) => error!("couldn't remove {file} from recipe: {e}"),
+                Err(e) => error!("couldn't remove file from recipe: {e}"),
             }
             info!("removing {file} from directory");
-            fs::remove_file(file).unwrap_or_else(|e| error!("couldn't remove file: {e}"));
+            fs::remove_file(file).unwrap_or_else(|e| warn!("couldn't remove file: {e}"));
         }
     }
 
     if let Some(ver) = ver {
-        info!("setting pkgver as {ver}, setting pkgrel as 1");
-        match Command::new("sed")
-            .args([
-                "-i",
-                "-e",
-                &format!("s|^pkgver=.*|pkgver={ver}|; s|^pkgrel=.*|pkgrel=1|"),
-                "PKGBUILD",
-            ])
-            .status()
-        {
+        info!("setting `pkgver` as {ver}, setting `pkgrel` as 1");
+        match sed(&format!(
+            "s|^pkgver=.*|pkgver={ver}|; s|^pkgrel=.*|pkgrel=1|"
+        )) {
             Ok(_) => (),
-            Err(e) => error!("couldn't sed PKGBUILD: {e}"),
+            Err(e) => error!("couldn't change `pkgver` and/or `pkgrel`: {e}"),
         }
     }
     if let Some(ver_git) = git {
         info!("setting commit as {ver_git}, setting pkgrel as 1");
-        match Command::new("sed")
-            .args([
-                "-i",
-                "-e",
-                &format!("s|^_commit=.*|_commit={ver_git}|; s|^pkgrel=.*|pkgrel=1|"),
-                "PKGBUILD",
-            ])
-            .status()
-        {
+        match sed(&format!(
+            "s|^_commit=.*|_commit={ver_git}|; s|^pkgrel=.*|pkgrel=1|"
+        )) {
             Ok(_) => (),
-            Err(e) => error!("couldn't sed PKGBUILD: {e}"),
+            Err(e) => error!("couldn't change commit SHA: {e}"),
         }
         if make.is_none() && make_mingw.is_none() {
             warn!("you may need to run `makepkg` manually to update `pkgver`");
         }
     }
     info!("updating checksums");
-    match Command::new("sh").arg("updpkgsums").status() {
-        Ok(_) => (),
-        Err(e) => error!("couldn't update checksums: {e}"),
+    // Windows doesn't support sh executables
+    if cfg!(windows) {
+        match Command::new("sh").arg("updpkgsums").status() {
+            Ok(_) => (),
+            Err(e) => error!("couldn't update checksums: {e}"),
+        }
+    } else {
+        match Command::new("updpkgsums").status() {
+            Ok(_) => (),
+            Err(e) => error!("couldn't update checksums: {e}"),
+        }
     }
 
     if make.is_some() && make_mingw.is_some() {
         error!("can't invoke both `makepkg` and `makepkg-mingw`");
     } else if let Some(flags) = make {
         info!("using `makepkg` with flags {flags}");
-        match Command::new("sh").args(["makepkg", &flags]).status() {
-            Ok(_) => (),
-            Err(e) => error!("couldn't make package: {e}"),
+        if cfg!(windows) {
+            match Command::new("sh").args(["makepkg", &flags]).status() {
+                Ok(_) => (),
+                Err(e) => error!("couldn't make package: {e}"),
+            }
+        } else {
+            match Command::new("makepkg").arg(&flags).status() {
+                Ok(_) => (),
+                Err(e) => error!("couldn't make package: {e}"),
+            }
         }
+    // MSYS2 only
     } else if let Some(flags) = make_mingw {
         info!("using `makepkg-mingw` with flags {flags}");
         match Command::new("sh").args(["makepkg-mingw", &flags]).status() {
